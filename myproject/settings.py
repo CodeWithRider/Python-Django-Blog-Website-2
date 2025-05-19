a16fecd8deb55f23597cd1a877d53e0c6df81482
CodeWithRider <priyankapal8823@gmail.com>
2025-05-20 01:50:04 +0530

    Prepare for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -17,13 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False").lower() in ["true", "1", "yes"]
-
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -88,6 +85,10 @@ DATABASES = {
     }
 }
 
+# Override with DATABASE_URL if provided (used on Render)
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
